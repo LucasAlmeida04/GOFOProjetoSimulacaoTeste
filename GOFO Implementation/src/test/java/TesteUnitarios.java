dae6657836c505180785629e45b4be9b2c15d3ba
LucasAlmeida04 <100705918+LucasAlmeida04@users.noreply.github.com>
2023-11-22 16:32:49 -0300

    Update TesteUnitarios.java

--- GOFO Implementation/src/test/java/TesteUnitarios.java
+++ GOFO Implementation/src/test/java/TesteUnitarios.java
@@ -1,1 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class TesteUnitarios {
+
+    private Player player;
+
+    @BeforeEach
+    public void setUp() {
+        player = new Player();
+    }
+
+    @Test
+    public void testSetAndGetBalance() {
+        // Definindo o saldo como 100
+        player.setBalance(100);
+
+        // Verificando se o saldo definido é igual a 100
+        assertEquals(100, player.getBalance());
+    }
+}
